8bf423d4f282822a54f28e39633bdc11f813aaf5
Ronal S. <ronal.sanx21@gmail.com>
2026-02-03 13:30:51 -0500

    config variables de entorno / conexion db pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({}),    
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+    UserModule
+  ],
   controllers: [],
   providers: [],
 })
