f4d076a7b6988b686c15fcaa3e2e675af2c5e3a6
Ronal S. <ronal.sanx21@gmail.com>
2026-02-03 13:37:51 -0500

    declaracion entidad usuario

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,4 +1,30 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: 'users'})
 export class User {
 
-    
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column('text')
+    nombres: string
+
+    @Column('text')
+    apellidos: string
+
+    @Column('text')
+    correo: string
+
+    @Column('text')
+    hash_password: string
+
+    @Column('bool')
+    activo: boolean
+
+    @Column('text')
+    telefono: string
+
+    @Column('text')
+    departamento: string
+
 }
