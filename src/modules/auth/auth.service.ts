0fb7264f42463a310472bf45ad0eb01856a105f2
Ronal S. <ronal.sanx21@gmail.com>
2026-02-06 18:01:51 -0500

    Instalacion de bcrypt y desinstalacion de dependencias de desarrollo

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { AuthDTO } from './dto/auth-dto';
-import { Repository } from 'typeorm';
-import { User } from '../user/entities/user.entity';
+import * as bcrypt from 'bcrypt'
 import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AuthDTO } from './dto/auth-dto';
+import { User } from '../user/entities/user.entity';
+
 import { UserService } from '../user/user.service';
+
+
 
 @Injectable()
 export class AuthService {
@@ -18,12 +22,16 @@ export class AuthService {
   
   async iniciarSesion(authDTO: AuthDTO){
 
-    const user = await this.userRepository.findOneBy({correo: authDTO.correo})
+    const { password, correo} = authDTO
+    const user = await this.userRepository.findOneBy({correo})
 
     if(!user) throw new UnauthorizedException('Correo y/o contraseña invalidos')
     
+    if(!await bcrypt.compare(authDTO.password, user.hash_password)) throw new UnauthorizedException('Correo y/o contraseña invalidos') 
     
+    // to-do generar jwt 
     
+    return user
     
   }
 }
